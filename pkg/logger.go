c9b35a8a1ee1765f1306a9634f60777d9b1c7013
yongjiachen <yongjia.chen@hotmail.com>
2021-12-06 13:03:40 +0800

    optimize logger

--- pkg/logger.go
+++ pkg/logger.go
@@ -14,16 +14,16 @@ import (
 var (
 	_Logger *logrus.Logger = &logrus.Logger{
 		Out: os.Stderr,
-		Formatter: ext.NewUTC8Formatter(&logrus.JSONFormatter{
+		Formatter: &logrus.JSONFormatter{
 			PrettyPrint: false,
 			CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
 				return ext.LogShortCaller(frame.Func.Name(), frame.Line), ""
 			},
-		}),
+		},
 		Hooks:        make(logrus.LevelHooks),
 		Level:        logrus.DebugLevel,
 		ExitFunc:     os.Exit,
-		ReportCaller: false,
+		ReportCaller: true,
 	}
 )
 
@@ -35,7 +35,7 @@ func SetLogger(logger *logrus.Logger) {
 	_Logger = logger
 }
 
-func InitLogger() error {
+func InitLogger() (*logrus.Logger, error) {
 	viper.SetDefault("app.log.path", "logs")
 	viper.SetDefault("app.log.level", "debug")
 	viper.SetDefault("app.log.format", "json")
@@ -67,13 +67,13 @@ func InitLogger() error {
 	}
 	level, err := logrus.ParseLevel(viper.GetString("app.log.level"))
 	if err != nil {
-		return fmt.Errorf("fatal parse log level: %w", err)
+		return nil, fmt.Errorf("fatal parse log level: %w", err)
 	}
 	dir := viper.GetString("app.log.path")
 	_ = os.Mkdir(dir, os.ModePerm)
 	file, err := os.OpenFile(dir+"/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return fmt.Errorf("fatal parse log file: %w", err)
+		return nil, fmt.Errorf("fatal parse log file: %w", err)
 	}
 	_Logger = &logrus.Logger{
 		Out:          io.MultiWriter(os.Stderr, file),
@@ -83,5 +83,5 @@ func InitLogger() error {
 		ExitFunc:     os.Exit,
 		ReportCaller: viper.GetBool("app.log.caller"),
 	}
-	return nil
+	return _Logger, nil
 }
